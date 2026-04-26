ed26edf11338e6c0fdff8d85efb607a11a904307
apiload5 <amir03182055632@gmail.com>
2026-04-26 19:47:37 +0500

    Change server port from 8000 to 8080

--- main.py
+++ main.py
@@ -175,4 +175,4 @@ async def download_video(url: str = Query(..., description="Video URL")):
         raise HTTPException(status_code=500, detail=f"Server error: {str(e)[:100]}")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
